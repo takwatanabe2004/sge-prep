38c2db5f098c7d5db1b1db58930769e0fb3c469e
Takanori Watanabe (sbia-pc125) <Takanori.Watanabe@uphs.upenn.edu>
2015-08-06 17:21:39 -0400

    Prepping up to cv-resamp for libsvm with gaussian kernel

---
+++ 0806-cvresamp-libsvm/readme.rst
@@ -0,0 +1,2 @@
+15-cv, 15-resamp grid search based on ``tw_0806_bal_gridsearch_vol_cv_resamp_libsvm.m``
+
--- util-shared/savemat_tester.m
+++
@@ -1,7 +0,0 @@
-clear
-
-time = tak_timestamp;
-[~,cwd] = system('pwd')
-outpath = strcat(cwd,'/timeNow')
-% outpath = ['/cbica/home/watanabt/vol_clf_ibis/timeNow']
-save(outpath,'time')
